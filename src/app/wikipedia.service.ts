acc415d90164886906b1d6f0a66895f0a43b292f
rohit panwar <rohitpanwarr@gmail.com>
2020-12-30 22:01:14 +0530

    Adde rxjx code and Added deployed file.

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
 
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      pageid: number,
+      title: string,
+      snippet: string,
+      wordcount: number
+    }[]
+  }
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +21,7 @@ export class WikipediaService {
   constructor(private http: HttpClient) { }
 
   public search(term: string) {
-    return this.http.get(
+    return this.http.get<WikipediaResponse>(
       'https://en.wikipedia.org/w/api.php',
       {
         params: {
@@ -20,7 +32,8 @@ export class WikipediaService {
           srsearch: term,
           origin: '*'
         }
-      }
-    );
+      }).pipe(
+        pluck('query', 'search')
+      );
   }
 }
